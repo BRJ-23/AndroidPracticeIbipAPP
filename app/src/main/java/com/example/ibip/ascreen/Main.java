bfd7423373fa70da58180d085cb5b4a75fd2fd2c
ChriBiels <72012125+ChriBiels@users.noreply.github.com>
2021-12-09 11:17:47 +0100

    Custom ListView Pepe Added

--- app/src/main/java/com/example/ibip/ascreen/Main.java
+++ app/src/main/java/com/example/ibip/ascreen/Main.java
@@ -154,6 +154,12 @@ public class Main extends AppCompatActivity {
         startActivity(i);
     }
 
+    public void listviewpepe(View view){
+        Intent i = new Intent(this, com.example.ibip.listViewPepe.MainActivity.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(i);
+    }
+
 
 
 }
